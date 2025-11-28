d48fed43fb5fb7e0de277287834ace6404c594f0
paragtms <parag.patel@thoughtmate.com>
2025-11-28 15:36:41 +0530

    Insert form data

--- index.js
+++ index.js
@@ -31,5 +31,42 @@ app.get('/update-cobj', (req, res) => {
     res.render('updates');
 });
 
+app.post('/update-cobj', async (req, res) => {
+    const { fname, lname, email } = req.body;
+    console.log(fname, lname, email)
+    try {
+        const response = await axios.post(
+            "https://api.hubapi.com/crm/v3/objects/contacts/search",
+
+            { "filterGroups": [{ "filters": [{ "operator": "EQ", "propertyName": "email", "value": email }] }] },
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+                    'Content-Type': 'application/json'
+                }
+            },
+
+        );
+        if (response.data.results.length > 0) {
+            console.log('user exist')
+        } else {
+            let res = await axios.post(
+                'https://api.hubspot.com/crm/v3/objects/contacts',
+                { properties: { firstname: fname, lastname: lname, email } },
+                {
+                    headers: {
+                        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+
+        }
+        res.redirect('/');
+    } catch (err) {
+        console.error('HubSpot error:', err);
+        res.status(500).send('Error creating new contact');
+    }
+});
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
